9e83b0591a3c9d7c397b9517395f2269abb99cee
PepMeister <davayte.sdohnem@yandex.ru>
2018-05-16 02:34:26 +0300

    os 1st lab, 4th var

---
+++ lab_1_danila.py
@@ -0,0 +1,119 @@
+#coding:UTF8
+from time import sleep
+
+class Task:
+	def __init__(self, taskName, actTime, execTime):
+		self.name = taskName
+		self.actTime = actTime
+		self.execTime = execTime
+		self.endOfExecutionTime = 0
+
+
+
+class Dispatcher(): #Task):
+	def __init__(self, tasks):
+		self.time = 0
+		self.task_list = tasks
+		self.currentTask =  Task("",0, self.task_list[0].actTime)
+		self.currentTask.endOfExecutionTime = self.currentTask.execTime
+		self.outOfTasks = False
+		self.quantum = 4
+		self.endOfquantumTime = self.currentTask.execTime
+
+
+	def inc_time(self):
+		self.time += 1
+
+
+	def skip(self, num):
+		for i in range(num):
+			print(str(self.time) + " " + "-")
+			self.time += 1
+			self.endOfquantumTime = self.currentTask.actTime + self.quantum
+
+
+	def print_task_state(self):
+		string = ""
+		for task in self.task_list:
+				if task.actTime <= self.time and not (self.currentTask.name == task.name):
+					string = string + " " + task.name
+		print(self.time, (" ")*(7+len("Имя")), self.currentTask.name, (" ")*(7+len("Задача")), string)
+
+
+
+	def check_current_task(self):
+		if self.currentTask.endOfExecutionTime == self.time:
+			if len(self.task_list) == 0:
+				self.outOfTasks = True
+				return
+
+			self.currentTask = self.task_list.pop(0)
+
+			for task in self.task_list:
+				try:
+					self.task_list.remove(self.currentTask)
+				except ValueError:
+					num = self.currentTask.actTime - self.time
+					self.skip(num)
+			self.currentTask.endOfExecutionTime = self.time + self.currentTask.execTime
+			self.endOfquantumTime = self.time + self.quantum
+
+
+		if self.endOfquantumTime == self.time:
+			print("истек квант процессорного времени, выполняется следующая задача" )
+			self.currentTask.execTime=(self.currentTask.endOfExecutionTime - self.time)
+			self.task_list.append(self.currentTask)
+
+			self.currentTask = self.task_list.pop(0)
+
+			for task in self.task_list:
+				try:
+					self.task_list.remove(self.currentTask)
+				except ValueError:
+					num = self.currentTask.actTime - self.time
+					#print("was", self.endOfquantumTime)
+					#if num != 0: 
+					#self.endOfquantumTime = self.currentTask.actTime + self.quantum
+					#print("now", self.endOfquantumTime)
+					self.skip(num)
+			self.currentTask.endOfExecutionTime = self.time + self.currentTask.execTime
+			self.endOfquantumTime = self.time + self.quantum
+
+
+
+
+
+
+def print_task_list(taskList):
+	for task in taskList:
+		print("Имя: " + str(task.name) + " " + " Время активизации: " + str(task.actTime) +
+			" Время выполнения: " + str(task.execTime))
+
+
+def run_modeling(tasks):
+	dispatcher = Dispatcher(tasks)
+	print("квант процессорного времени равен: ", dispatcher.quantum, "\n" )
+	print('Время' + (" ")*7 + "Задача" + (" ")*7 + " Очередь")
+
+	while not dispatcher.outOfTasks:
+		sleep(0.1)
+		dispatcher.check_current_task()
+		dispatcher.print_task_state()
+		dispatcher.inc_time()
+
+
+
+file = open("input_", 'r') 
+tasks = [] 
+for line in file: 
+	data = line.split() 
+	taskName = data[0] 
+	actTime = int(data[1])
+	execTime = int(data[2])
+
+	tasks.append(Task(taskName, actTime, execTime))
+
+
+tasks = sorted(tasks, key=lambda x: x.actTime)
+print_task_list(tasks) 
+run_modeling(tasks)
